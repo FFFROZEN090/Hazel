f5e2b9382acc2cf823e1983ef176efc9f00ba899
FFFROZEN <839814732@qq.com>
2022-04-02 13:08:10 +0800

    Setup basci application and entry point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//For use by Hazel Application
+#include "Hazel/Application.h"
+#include <stdio.h>
+
+
+// ---------------------Entry Point---------------------
+#include "Hazel/EntryPoint.h"
+// -----------------------------------------------------
